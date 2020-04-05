206f06347516fc02ec751b755274b5a211fda1fc
Marco Oliveira <marcomoa@hotmail.com>
2020-04-04 22:58:29 -0300

    src: change serverID to clientID

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -2382,7 +2382,7 @@ void ProtocolGame::sendContainer(uint8_t cid, const Container* container, bool h
 
 	playermsg.addByte(cid);
 	if (container->getID() == ITEM_BROWSEFIELD) {
-		AddItem(1987, 1);
+		AddItem(ITEM_BAG, 1);
 		playermsg.addString("Browse Field");
 	} else {
 		AddItem(container);
